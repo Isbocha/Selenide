be4e907bbfe049dc99f40b6093fe75d1db166eed
InnaB <isbocha@gmail.com>
2023-08-17 21:35:29 +0300

    Add correct test

--- src/test/java/ru/netology/selenide/FormCardTest.java
+++ src/test/java/ru/netology/selenide/FormCardTest.java
@@ -1,10 +1,6 @@
 package ru.netology.selenide;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
